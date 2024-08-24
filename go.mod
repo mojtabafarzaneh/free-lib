8aa73f2f733e82bb95b9afda0bd1a7dda35e8d6f
mojtaba <mojtabafarzaneh78@gmail.com>
2024-08-24 13:04:49 +0330

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mojtabafarzaneh/free-lib
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,8 @@
+package main 
+
+import "fmt"
+
+func main(){
+	fmt.Println("let's do something for open software comunity")
+
+}
